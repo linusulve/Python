8b099bb787a68b68f4300601201d17595b3cf9b7
nothingspecial <59864643+linusulve@users.noreply.github.com>
2020-08-19 20:26:27 +0200

    Add files via upload

---
+++ Python/Game.py
@@ -0,0 +1,28 @@
+Player_1 = "Sam"
+Player_2 = "Alex"
+Current_round = 1
+
+print("Game on!")
+
+print(f"player 1: {Player_1}")
+print(f"player 2: {Player_2}")
+
+print("---------------------------")
+
+print(f"round: {Current_round}")
+
+Player_1_score = 10
+Player_2_score = 13
+
+print(f"player 1 score = {Player_1_score}")
+print(f"player 2 score = {Player_2_score}")
+
+print(f"{Player_2} Wins with {Player_2_score} to {Player_1_score}")
+
+print("---------------------------")
+
+print(f"Round {Current_round +1}!")
+player_1_score = 20
+player_2_score = 5
+
+print(f"{Player_1} defeats {Player_2} by {Player_1_score - Player_2_score}")
